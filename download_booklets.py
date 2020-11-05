c68f147b5df17a2b6831832efec2149f2f52ed1b
Sahircan Sürmeli <ssahircan@gmail.com>
2020-11-05 21:52:36 +0300

    Add feedback

--- download_booklets.py
+++ download_booklets.py
@@ -7,6 +7,8 @@ doc = lxml.html.fromstring(r.content)
 links = doc.xpath("//ul/li/a[contains(@href, '.pdf') and not(contains(@href, 'TSUNAMI')) and not(contains(@href, 'TSUNAMi'))]")
 
 for l in links:
+    print("Start download " + l.attrib["title"])
     download = requests.get(l.attrib["href"])
     with open(f"{l.attrib['title']}.pdf", "wb") as file:
         file.write(download.content)
+    print("Download completed " + l.attrib["title"])
