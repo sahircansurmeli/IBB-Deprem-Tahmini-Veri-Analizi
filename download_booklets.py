cdd57af1b395362822e96006fd548ded073cfd70
Sahircan Sürmeli <ssahircan@gmail.com>
2020-11-05 21:48:09 +0300

    first commit

---
+++ download_booklets.py
@@ -0,0 +1,12 @@
+import requests
+import lxml.html
+# from bs4 import BeautifulSoup
+
+r = requests.get("https://depremzemin.ibb.istanbul/guncelcalismalarimiz/")
+doc = lxml.html.fromstring(r.content)
+links = doc.xpath("//ul/li/a[contains(@href, '.pdf') and not(contains(@href, 'TSUNAMI')) and not(contains(@href, 'TSUNAMi'))]")
+
+for l in links:
+    download = requests.get(l.attrib["href"])
+    with open(f"{l.attrib['title']}.pdf", "wb") as file:
+        file.write(download.content)
